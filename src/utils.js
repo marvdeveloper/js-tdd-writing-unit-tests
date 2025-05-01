d6216772a7ed8b7ed8254f43012c93be87a69406
Marvin Kadoyo <kadoyomarvin@gmail.com>
2025-05-01 22:35:03 +0300

    Updated pointsForWord function to handle case-insensitive vowels

--- src/utils.js
+++ src/utils.js
@@ -1,1 +1,10 @@
-// Your code here
+
+export function pointsForWord(word) {
+    let points = 0;
+    for (const char of word) {
+      // Using the 'i' flag for case insensitivity, checking for vowels
+      points += /[aeiou]/i.test(char) ? 1 : 2;  
+    }
+    return points;
+  }
+  
